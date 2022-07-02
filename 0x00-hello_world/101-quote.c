a27e251def67ac4cebd54e7de335daa2e48e5c10
olofindamilola <kdammy2017@gmail.com>
2022-07-02 01:40:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * description: print with proper grammar, but the outcome is a piece of art
- * Return: Always 0 (Success)
+ * description: prints Dora's quote
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n ", 59);
+	retunr (1);
 }
