ac3834fdcb3a949b0b39c17d1295c5d8c6dc9ffc
olofindamilola <kdammy2017@gmail.com>
2022-07-21 01:01:20 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,9 @@
+#ifndef _MAIN_H
+
+
+
+#define _MAIN_H
+
 int _putchar(char c);
 
 void _puts_recursion(char *s);
@@ -9,3 +15,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
+#endif
